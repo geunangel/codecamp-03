f595c43e500049b432370976da650f635cb5e688
geunangel <1004geuna1004@gmail.com>
2021-09-12 23:50:11 +0900

    update

--- freeboard_frontend/src/units/board/list/BoardList.presenter.js
+++ freeboard_frontend/src/units/board/list/BoardList.presenter.js
@@ -1,63 +1,79 @@
 import {
-    Wrapper,Header,
-    BestBoards,BoardsImg,BoardTitle,
-    WriterBoardBox,WriterBox,WriterImg,WriterName
-} from './BoardList.styles'
+  Wrapper,
+  Header,
+  BestBoards,
+  BoardsImg,
+  BoardTitle,
+  WriterBoardBox,
+  WriterBox,
+  WriterImg,
+  WriterName,
+  BoardListBox,
+  BoardListTop,
+  BoardListBottom,
+  BoardListNumber,
+  BoardListTitle,
+  BoardListwriter,
+  BoardLisDay,
+} from "./BoardList.styles";
 
-export default function BoardListPresenter(props){
-    return(
-        <Wrapper>
-            <Header>베스트 게시글</Header>
-            <BestBoards>
-                <BoardsImg />
-                <BoardTitle>게시물 제목입니다.</BoardTitle>
-                <WriterBoardBox>
-                    <WriterBox>
-                        <WriterImg>사진</WriterImg>
-                        <WriterName>노원두</WriterName>
-                    </WriterBox>
-                    <div>날짜</div>
-                    <div>
-                        <div>따봉</div>
-                        <div>356</div>
-                    </div>
-                </WriterBoardBox>
-            </BestBoards>
-            <div>
-                <div>
-                    <img />
-                    <input />제목을 검색해주세요.
-                </div>
-                <div>년도입력</div>
-                <div>검색하기</div>
+export default function BoardListPresenter(props) {
+  return (
+    <Wrapper>
+      <Header>베스트 게시글</Header>
+      <BestBoards>
+        <BoardsImg />
+        <BoardTitle>게시물 제목입니다.</BoardTitle>
+        <WriterBoardBox>
+          <WriterBox>
+            <WriterImg>사진</WriterImg>
+            <WriterName>노원두</WriterName>
+          </WriterBox>
+          <div>날짜</div>
+          <div>
+            <div>따봉</div>
+            <div>356</div>
+          </div>
+        </WriterBoardBox>
+      </BestBoards>
+      <div>
+        <div>
+          <img />
+          <input />
+          제목을 검색해주세요.
+        </div>
+        <div>년도입력</div>
+        <div>검색하기</div>
+      </div>
+      <BoardListBox>
+        <BoardListTop>
+          <BoardListNumber>번호</BoardListNumber>
+          <BoardListTitle>제목</BoardListTitle>
+          <BoardListwriter>작성자</BoardListwriter>
+          <BoardLisDay>날짜</BoardLisDay>
+        </BoardListTop>
+        <BoardListBottom>
+          {props.data?.fetchBoards.map((el, index) => (
+            <div key={el._id}>
+              <BoardListNumber>{index + 1}</BoardListNumber>
+              <BoardListTitle>{el.title}</BoardListTitle>
+              <BoardListwriter>{el.writer}</BoardListwriter>
+              <BoardLisDay>{el.createdAt}</BoardLisDay>
             </div>
-            <div>
-                <div>번호</div>
-                <div>제목</div>
-                <div>작성자</div>
-                <div>날짜</div>
-            </div>
-            <div>
-                {props.data?.fetchBoards.map((el, index) => (
-                    <div key={el._id}>
-                        <div>{index+1}</div>
-                        <div>{el.title}</div>
-                        <div>{el.writer}</div>
-                        <div>{el.createdAt}</div>
-                    </div>
-                ))}
-            </div>
-            <div>
-                <div>
-                    <div>앞</div>
-                    <div>1</div>
-                    <div>뒤</div>
-                </div>
-                <div>
-                    <img />
-                    <div>게시물 등록하기</div>
-                </div>
-            </div>
-        </Wrapper>
-    )
+          ))}
+        </BoardListBottom>
+      </BoardListBox>
+      <div>
+        <div>
+          <div>앞</div>
+          <div>1</div>
+          <div>뒤</div>
+        </div>
+        <div>
+          <img />
+          <div>게시물 등록하기</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
--- freeboard_frontend/src/units/board/read/BoardRead.presenter.js
+++ freeboard_frontend/src/units/board/read/BoardRead.presenter.js
@@ -1,104 +1,129 @@
 import {
-    Wrapper,Detail,Header,Footer,Head1,Head2,
-    Title,Img,Contents,Youtube,
-    Likedislike,Like,Likecount,Dislike,Dislikecount,Count,
-    Box,WriterImg,
-    ButtonBox,ButtonList,ButtonEdit,ButtonDelete,
-    CommentBox,CommentIcon,CommentText,CommentData,
-    CommentWriter,CommentPw,CommentStarbox,CommentStar,
-    CommentContentsBox,CommentContents,ButtonComment
-} from './BoardRead.styles'
+  Wrapper,
+  Detail,
+  Header,
+  Footer,
+  Head1,
+  Head2,
+  Title,
+  Img,
+  Contents,
+  Youtube,
+  Likedislike,
+  Like,
+  Likecount,
+  Dislike,
+  Dislikecount,
+  Count,
+  Box,
+  WriterImg,
+  ButtonBox,
+  ButtonList,
+  ButtonEdit,
+  ButtonDelete,
+  CommentBox,
+  CommentIcon,
+  CommentText,
+  CommentData,
+  CommentWriter,
+  CommentPw,
+  CommentStarbox,
+  CommentStar,
+  CommentContentsBox,
+  CommentContents,
+  ButtonComment,
+} from "./BoardRead.styles";
 
-
-export default function BoardReadPresenter(props){
-
-    return(
-        <Wrapper>
-            <Detail>
-            <Header>
-                <Head1>
-                    <WriterImg>
-                        <div>이미지</div>
-                    </WriterImg>
-                    <div>
-                        <div>{props.data?.fetchBoard.writer}</div>
-                        <div>2021.02.18</div>
-                    </div>
-                </Head1>
-                <Head2>
-                    <div>위치글자</div>
-                    <div>아이콘</div>
-                    <div>아이콘</div>
-                </Head2>
-            </Header>
-            <Footer>
-                <Title>{props.data?.fetchBoard.title}</Title>
-                <Box>
-                    <Img>사진</Img>
-                    <Contents>{props.data?.fetchBoard.contents}</Contents>
-                    <Youtube></Youtube>
-                    <Likedislike>
-                        <Likecount>
-                            <Like></Like>
-                            <Count>1920</Count>
-                        </Likecount>
-                        <Dislikecount>
-                            <Dislike></Dislike>
-                            <Count>1920</Count>
-                        </Dislikecount>
-                    </Likedislike>
-                </Box>
-            </Footer>
-            </Detail>
-            <ButtonBox>
-                <ButtonList onClick={props.onClickList}>목록으로</ButtonList>
-                <ButtonEdit onClick={props.onClickEdit}>수정하기</ButtonEdit>
-                <ButtonDelete onClick={props.onClickDelete}>삭제하기</ButtonDelete>
-            </ButtonBox>
+export default function BoardReadPresenter(props) {
+  return (
+    <Wrapper>
+      <Detail>
+        <Header>
+          <Head1>
+            <WriterImg>
+              <div>이미지</div>
+            </WriterImg>
+            <div>
+              <div>{props.data?.fetchBoard.writer}</div>
+              <div>2021.02.18</div>
+            </div>
+          </Head1>
+          <Head2>
+            <div>위치글자</div>
+            <div>아이콘</div>
+            <div>아이콘</div>
+          </Head2>
+        </Header>
+        <Footer>
+          <Title>{props.data?.fetchBoard.title}</Title>
+          <Box>
+            <Img>사진</Img>
+            <Contents>{props.data?.fetchBoard.contents}</Contents>
+            <Youtube></Youtube>
+            <Likedislike>
+              <Likecount>
+                <Like></Like>
+                <Count>1920</Count>
+              </Likecount>
+              <Dislikecount>
+                <Dislike></Dislike>
+                <Count>1920</Count>
+              </Dislikecount>
+            </Likedislike>
+          </Box>
+        </Footer>
+      </Detail>
+      <ButtonBox>
+        <ButtonList onClick={props.onClickList}>목록으로</ButtonList>
+        <ButtonEdit onClick={props.onClickEdit}>수정하기</ButtonEdit>
+        <ButtonDelete onClick={props.onClickDelete}>삭제하기</ButtonDelete>
+      </ButtonBox>
+      <br />
+      <CommentBox>
+        <CommentIcon></CommentIcon>
+        <CommentText>댓글</CommentText>
+        <CommentData>
+          <CommentWriter placeholder="작성자" onChange={props.onChangeWriter} />
+          <CommentPw placeholder="비밀번호" onChange={props.onChangePassword} />
+          <CommentStarbox>
+            <CommentStar>별</CommentStar>
+            <CommentStar>별</CommentStar>
+            <CommentStar>별</CommentStar>
+            <CommentStar>별</CommentStar>
+          </CommentStarbox>
+        </CommentData>
+        <CommentContentsBox>
+          <CommentContents
+            placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제 될 수 있으며, 이에 대한 민사책임은 게시자에게 있습니다"
+            onChange={props.onChangeContents}
+          />
+          <div>
+            <div></div>
+            <ButtonComment onClick={props.onClickComment}>
+              등록하기
+            </ButtonComment>
+          </div>
+        </CommentContentsBox>
+        <div>
+          <div>
+            <div>이미지</div>
+            <div>
+              <div>사진</div>
+              <div>
+                <div>별</div>
+                <div>별</div>
+                <div>별</div>
+                <div>별</div>
+                <div>별</div>
+              </div>
+              <div>수정하기아이콘</div>
+              <div>삭제하기아이콘</div>
+            </div>
+            <div>내용</div>
             <br />
-            <CommentBox>
-                <CommentIcon></CommentIcon>
-                <CommentText>댓글</CommentText>
-                <CommentData>
-                    <CommentWriter placeholder="작성자" onChange={props.onChangeWriter} />
-                    <CommentPw placeholder="비밀번호" onChange={props.onChangePassword} />
-                    <CommentStarbox>
-                        <CommentStar>별</CommentStar>
-                        <CommentStar>별</CommentStar>
-                        <CommentStar>별</CommentStar>
-                        <CommentStar>별</CommentStar>
-                    </CommentStarbox>
-                </CommentData>
-                <CommentContentsBox>
-                    <CommentContents 
-                        placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제 될 수 있으며, 이에 대한 민사책임은 게시자에게 있습니다" 
-                        onChange={props.onChangeContents}
-                    />
-                    <div>
-                        <div></div>
-                        <ButtonComment onClick={props.onClickComment}>등록하기</ButtonComment>
-                    </div>
-                </CommentContentsBox>
-                <div>
-                    <div>
-                        <div>이미지</div>
-                        <div>
-                            <div>사진</div>
-                            <div>
-                                <div>별</div>
-                                <div>별</div>
-                                <div>별</div>
-                                <div>별</div>
-                                <div>별</div>
-                            </div>
-                            <div>수정하기아이콘</div>
-                            <div>삭제하기아이콘</div>
-                        </div>
-                        <div>내용</div>
-                        <br />
-                    </div>
-                </div>
-            </CommentBox>
-        </Wrapper>
-    )
+          </div>
+        </div>
+      </CommentBox>
+    </Wrapper>
+  );
 }
