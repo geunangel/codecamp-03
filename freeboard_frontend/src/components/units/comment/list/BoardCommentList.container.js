889e78df00adddd585c4884e936bb2183938fcda
geunangel <1004geuna1004@gmail.com>
2021-09-26 23:25:39 +0900

    comment infinite scroll update

--- freeboard_frontend/src/components/units/comment/list/BoardCommentList.container.js
+++ freeboard_frontend/src/components/units/comment/list/BoardCommentList.container.js
@@ -5,10 +5,26 @@ import { FETCH_BOARD_COMMENTS } from "./BoardCommentList.queries";
 
 export default function BoardCommentContainer() {
   const router = useRouter();
-  const { data } = useQuery(FETCH_BOARD_COMMENTS, {
+  const { data, fetchMore } = useQuery(FETCH_BOARD_COMMENTS, {
     variables: { boardId: router.query.detail },
   });
   console.log(router);
 
-  return <BoardCommentPresenter data={data} />;
+  function onAllList() {
+    if (!data) return;
+
+    fetchMore({
+      variables: { page: Math.ceil(data?.fetchBoardComments.length / 10) + 1 },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        return {
+          fetchBoardComments: [
+            ...prev.fetchBoardComments,
+            ...fetchMoreResult.fetchBoardComments,
+          ],
+        };
+      },
+    });
+  }
+
+  return <BoardCommentPresenter data={data} onAllList={onAllList} />;
 }
--- freeboard_frontend/src/components/units/comment/list/BoardCommentList.presenter.js
+++ freeboard_frontend/src/components/units/comment/list/BoardCommentList.presenter.js
@@ -1,11 +1,12 @@
 import ListPresenterItem from "./BoardCommentList.presenterItem";
+import InfiniteScroll from "react-infinite-scroller";
 
 export default function BoardCommentPresenter(props) {
   return (
-    <>
+    <InfiniteScroll pageStart={0} loadMore={props.onAllList} hasMore={true}>
       {props.data?.fetchBoardComments.map((el) => (
         <ListPresenterItem key={el._id} el={el} />
       ))}
-    </>
+    </InfiniteScroll>
   );
 }
