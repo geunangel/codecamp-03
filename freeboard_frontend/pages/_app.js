ee85ed96d97160cc116017a9326af8b9141211da
geunangel <1004geuna1004@gmail.com>
2021-10-23 23:52:46 +0900

    market login update

--- freeboard_frontend/pages/_app.js
+++ freeboard_frontend/pages/_app.js
@@ -11,13 +11,13 @@ import { createUploadLink } from "apollo-upload-client";
 import { createContext, useState, useEffect } from "react";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalstyles";
+import { getAccessToken } from "../src/commons/libraries/validations/getAccessToken";
 
 export const GlobalContext = createContext(null);
 // function MyApp({ Component, pageProps }: AppProps)
 function MyApp({ Component, pageProps }) {
   const [accessToken, setAccessToken] = useState("");
-  const [userInfo, setUserInfo] = useState({});
-
+  const [userInfo, setUserInfo] = useState("");
   const value = {
     accessToken: accessToken,
     setAccessToken: setAccessToken,
@@ -25,28 +25,35 @@ function MyApp({ Component, pageProps }) {
     setUserInfo: setUserInfo,
   };
 
-  // useEffect(() => {
-  //   const accessToken = localStorage.getItem("accessToken") || "";
-  //   setAccessToken(accessToken);
-  // }, []);
+  useEffect(() => {
+    //   const accessToken = localStorage.getItem("accessToken") || "";
+    //   setAccessToken(accessToken);
+    if (localStorage.getItem("refreshToken")) getAccessToken(setAccessToken);
+  }, []);
 
-  //위,아래와 동일한 결과
-  // if(process.browser){
-  //   localStorage
-  // }else{
-  //   return ""
-  // }
+  //실패했을때 재발급 요청
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        if (err.extensions?.code === "UNAUTHENTICATED") {
+          // operation=>기존에 날렸던 쿼리
+          operation.setContext({
+            Headers: {
+              ...operation.getContext().headers,
+              authorization: `Bearer ${getAccessToken(setAccessToken)}`,
+            },
+          });
 
-  // if (typeofwindow !== undefined) {
-  //   return "";
-  // }
+          return forward(operation);
+        }
+      }
+    }
+  });
 
   const uploadLink = createUploadLink({
     uri: "http://backend03.codebootcamp.co.kr/graphql",
-    //로그인처리 어떻게 하셨나요?
-    //프론트에서 로그인해서 받은 엑세스 토큰  http  headers 리퀘스트에  authorization에 Bearer타입으로 엑세스토큰
-    // headers: { authorization: "Bearer 액세스토큰" },
     headers: { authorization: `Bearer ${accessToken}` },
+    credentials: "include",
   });
 
   //임시로 썼음
@@ -55,8 +62,7 @@ function MyApp({ Component, pageProps }) {
   // }
 
   const client = new ApolloClient({
-    link: ApolloLink.from([uploadLink]),
-    // link: ApolloLink.from([uploadLink as unknown as ApolloLink]),
+    link: ApolloLink.from([errorLink, uploadLink]),
     cache: new InMemoryCache(),
   });
 
--- freeboard_frontend/src/components/commons/hocs/WithAuth.js
+++ freeboard_frontend/src/components/commons/hocs/WithAuth.js
@@ -4,18 +4,17 @@ import { useRouter } from "next/router";
 
 export const WithAuth = (Component) => (props) => {
   const router = useRouter();
-  const { accessToken } = useContext(GlobalContext);
 
   //accessToken이 있는가!?
   //accessToken이 없다면, login 페이지로 보내기!!!
   useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
+    const accessToken = localStorage.getItem("refreshToken");
     console.log(accessToken);
     if (!accessToken) {
       alert("로그인후 이용해주세요");
       router.push("/signin");
     }
-  }, [accessToken]);
+  }, []);
 
   return <Component {...props} />;
 };
