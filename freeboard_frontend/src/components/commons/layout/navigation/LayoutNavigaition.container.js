c27bb72034f74abb64d68727605afc8b3c11314b
geunangel <1004geuna1004@gmail.com>
2021-10-31 23:48:59 +0900

    update

--- freeboard_frontend/src/components/commons/layout/navigation/LayoutNavigaition.container.js
+++ freeboard_frontend/src/components/commons/layout/navigation/LayoutNavigaition.container.js
@@ -1,34 +1,36 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 import LayoutNaigaitionUI from "./LayoutNavigaition.presenter";
 
 export default function LayoutHeader() {
   const router = useRouter();
+  const [isSelect, setIsSelect] = useState(false);
 
   const onClickMarketNew = () => {
     router.push("/market/new");
-    // setIsSelect(true);
+    setIsSelect(true);
   };
   const onClickMarketList = () => {
     router.push("/market/list");
-    // setIsSelect(true);
+    setIsSelect(true);
   };
 
   const onClickMarketBest = () => {
     router.push("/market/best");
-    // setIsSelect(true);
+    setIsSelect(true);
   };
 
   const onClickBoardList = () => {
     router.push("/boards");
-    // setIsSelect(true);
+    setIsSelect(true);
   };
 
   return (
     <LayoutNaigaitionUI
       onClickMarketNew={onClickMarketNew}
       onClickMarketList={onClickMarketList}
-      onClickBoardList={onClickBoardList}
       onClickMarketBest={onClickMarketBest}
+      onClickBoardList={onClickBoardList}
     />
   );
 }
