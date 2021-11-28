8ffbcb399961bf5a9825f9a8401a9854a0e488f1
geunangel <1004geuna1004@gmail.com>
2021-11-28 20:14:41 +0900

    market mypage 수정

--- freeboard_frontend/src/components/units/mypage/Mypage.presenter.js
+++ freeboard_frontend/src/components/units/mypage/Mypage.presenter.js
@@ -1,5 +1,5 @@
 import { Modal } from "antd";
-import { Pay } from "./Mypage.styles";
+import { Pay, UserInfo, UserName } from "./Mypage.styles";
 import Payment from "../market/payment/Payment.container";
 export default function MyPageUI(props) {
   return (
@@ -13,12 +13,12 @@ export default function MyPageUI(props) {
           <Payment />
         </Modal>
       )}
-      <div>나의 신상정보</div>
-      <div>이름:{props.data?.fetchUserLoggedIn.name}</div>
+      <UserInfo>나의 신상정보</UserInfo>
+      <UserName>이름:{props.data?.fetchUserLoggedIn.name}</UserName>
       <div>포인트</div>
       {props.data?.fetchUserLoggedIn.userPoint.amount}
       <div>내프로필</div>
-      <Pay onClick={props.onClickPayment}></Pay>
+      <Pay onClick={props.onClickPayment}>포인트충전하기</Pay>
       <div>구매내역</div>
     </div>
   );
--- freeboard_frontend/src/components/units/mypage/Mypage.styles.js
+++ freeboard_frontend/src/components/units/mypage/Mypage.styles.js
@@ -1,8 +1,18 @@
 import styled from "@emotion/styled";
 
 export const Pay = styled.button`
-  width: 100px;
-  height: 100px;
+  width: 120px;
+  height: 50px;
   background-color: beige;
   cursor: pointer;
+`;
+
+export const UserInfo = styled.div`
+  font-size: 50px;
+  font-weight: bold;
+`;
+
+export const UserName = styled.div`
+  font-size: 20px;
+  font-weight: bold;
 `;
